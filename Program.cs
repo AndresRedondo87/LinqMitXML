8d2915b7dad8f8af014d1c08bbd4db207125b8be
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-11-04 12:02:21 +0100

    Linq mit XML.... Tabelle mit ein String definieren, die Verbatim am Anfang "@"" nicht vergessen!!! orderby Int32.Parse(student.Age) Korrigiert! sonst falsch sortiert!

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LinqMitXML
 {
@@ -10,6 +11,93 @@ namespace LinqMitXML
     {
         static void Main(string[] args)
         {
+            string studentsXML =
+                        @"<Students>
+                            <Student>
+                                <Name>Toni</Name>
+                                <Age>21</Age>
+                                <University>Yale</University>
+                                <Semester>9</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Carla</Name>
+                                <Age>17</Age>
+                                <University>Yale</University>
+                                <Semester>8</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Leyla</Name>
+                                <Age>19</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>7</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>25</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>N.A.</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>1000</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>N.A.</Semester>
+                            </Student>                            
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>112</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>N.A.</Semester>
+                            </Student>
+                        </Students>";
+            /// jede einzelne Student ist eine Zeile von unsere Tabelle,
+            /// Name, Age, Uni, Semester sind die Spalten von JEDE Zeile.
+            /// so ist eine alternativ unsere Tabellen in ein String zu speichern
+            /// Diese TAbelle ist wie das letzte aber hat kein Gender.
+            /// 
+            /// video hat auch eine erklärung über generelle XML Datei...nicht so hilfreich
+
+            // Daten auslesen... ( using System.Xml.Linq; erforderlich)
+            XDocument studentsXdoc = new XDocument();
+            studentsXdoc = XDocument.Parse(studentsXML);    //hiermit lesen wir den string als XML aus
+
+            var students = from student in studentsXdoc.Descendants("Student")
+                               //descendants sucht die untergeordnete Objekte in der XML mit den Name die wir übernehmen
+                           select new
+                           {
+                               // wir wählen für jede Spalte in unsere Kollektion den Wert(Value) aus den entsprechenden Objekte(Element)
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value,
+                               Semester = student.Element("Semester").Value
+                               /// HIER AUFPASSEN DASS DIE NAMEN PERFEKT GESCHRIEBEN SIND,
+                               /// wenn möglich am besten via copy paste übernehmen
+                           };
+            /// wenn wir wieder über "var students" den maus halten, sehen wir den anonyme Typ
+            /// mit Name, Age, University properties.
+            /// Jetzt können wir die Properties als objekte verwenden!
+            /// 
+
+            foreach (var stu in students)
+            {
+                Console.WriteLine($"Der Student/in {stu.Name} ist {stu.Age} und studiert in {stu.University} und ist in Semester {stu.Semester}");
+            }
+            /// Herausforderung...? Semester Hinzufügen. etwas viel zu einfach...
+            /// .
+            /// 
+            /// Herausforderung 2 sortieren nach ihrem alter (copypaste + erneut darstellen
+            /// hat geklappt, hatte nur die originale Kollektio wieder ausgegeben, astatt die sortierte
+
+            //var ageSortedStudents = from student in students orderby student.Age select student;
+            // falsch sortiert mit Age als string, hier unter korrigiert. MACHT ABER EXCEPTION WENN AGE nicht geparsed sein kann TODO!!
+            var ageSortedStudents = from student in students orderby Int32.Parse(student.Age) select student;
+
+            Console.WriteLine("SORTIERT NACH ALTER");
+            foreach (var stu in ageSortedStudents)      
+            {
+                Console.WriteLine($"Der Student/in {stu.Name} ist {stu.Age} und studiert in {stu.University} und ist in Semester {stu.Semester}");
+            }
+            Console.ReadKey();
         }
     }
 }
